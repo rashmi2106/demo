95076854969999673de5cc108949c3ebd0200c01
rashmi2106 <rashminaiyyer@gmail.com>
2023-02-21 12:35:08 +0530

    scala programm using val and print

---
+++ src/test/scala/demo/Assignment1.scala
@@ -0,0 +1,12 @@
+package demo
+import scala.io.StdIn._
+
+object Assignment1 {
+  def main(args: Array[String]): Unit={
+    val name=readLine("Enter your name:")
+    val surname=readLine("Enter your Surname:")
+    print("Name"+name+"Surname"+surname)
+
+  }
+
+}
---
+++ src/test/scala/demo/input_if.scala
@@ -0,0 +1,11 @@
+package demo
+import scala.io.StdIn._
+
+object input_if {
+  def main(args: Array[String]): Unit={
+    print("Enter the number:")
+    val name=readInt()
+    print(name)
+  }
+
+}
